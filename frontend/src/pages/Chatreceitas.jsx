ea0157f5694aff440c599fd0841da66068ae46a3
fabricio rafael <142851247+fabriciorafael@users.noreply.github.com>
2026-09-24 22:09:22 -0300

    Configura Tailwind CSS no projeto

---
+++ frontend/src/pages/Chatreceitas.jsx
@@ -0,0 +1,22 @@
+const Chatreceitas = () => {
+
+
+    return (
+        <div className="min-h-screen bg-linear-to-br from-purplre-500 via-gray-50 to-emerald-50 p-4">
+            <div className="container mx-auto">
+
+                <header>
+
+                    <h1>CooKflow</h1>
+                    <p>Seu assistente de receitas favorito!</p>
+                </header>
+            </div>
+
+
+
+        </div>
+    )
+
+}
+
+export default Chatreceitas
